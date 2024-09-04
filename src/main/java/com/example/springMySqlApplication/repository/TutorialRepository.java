f6b1ad9b6fe983e8dddcf12102fadcdd8472299e
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-09-04 09:00:09 -0400

    Modified for lab 310.1.1

--- src/main/java/com/example/springMySqlApplication/repository/TutorialRepository.java
+++ src/main/java/com/example/springMySqlApplication/repository/TutorialRepository.java
@@ -2,9 +2,12 @@ package com.example.springMySqlApplication.repository;
 
 import com.example.springMySqlApplication.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
 }
